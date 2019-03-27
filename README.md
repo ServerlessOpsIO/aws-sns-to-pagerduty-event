97e8ffd0fe9cd784fd2c1c32126d3553c3836612
Tom McLaughlin <tom@serverlessops.io>
2019-03-26 20:19:21 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# aws-sns-to-pagerduty-event
+[![Serverless](http://public.serverless.com/badges/v3.svg)](http://www.serverless.com)
+[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)
+[![Build Status](https://travis-ci.org/ServerlessOpsIO/aws-sns-to-pagerduty-event.svg?branch=master)](https://travis-ci.org/ServerlessOpsIO/aws-sns-to-pagerduty-event)
+
+Publish a message received from SNS to PagerDuty as an event.
+
+This service provides an SNS topic with a Lambda function as a subscriber. A message published to this topic will be published to the PagerDuty Events API.
+
+## Service Interface
+
+* Event Type: AWS SNS
+* SNS Message: The value of the SNS Message attribute will be passed directly to PagerDuty as formatted in the SNS message.
+
+## Configuration
+
+The following Parameters are available.
+
+* __PagerDutyApiKey__: API Key for using the PagerDuty Events V2 API.
+* __PagerDutyIntegrationKey__: Integration key for the PagerDuty integration the event will be routed to.
+* __PagerDutySeverity__ (optional): Set the severity of the event. A default is provided.
+* __PagerDutySource__ (optional): Set the name of the source as show in PagerDuty. A default is provided.
+
---
+++ handlers/pagerduty_event_publisher.py
@@ -0,0 +1,58 @@
+'''Publish message from SNS to Slack'''
+
+import json
+import logging
+import os
+
+import pypd
+
+log_level = os.environ.get('LOG_LEVEL', 'INFO')
+logging.root.setLevel(logging.getLevelName(log_level))  # type: ignore
+_logger = logging.getLogger(__name__)
+
+pypd.api_key = os.environ.get('PD_API_KEY')
+PD_INT_KEY = os.environ.get('PD_INT_KEY')
+PD_SEVERITY = os.environ.get('PD_SEVERITY')
+PD_SOURCE = os.environ.get('PD_SOURCE')
+
+
+def _get_message_from_event(event: dict) -> str:
+    '''Get the message from the event'''
+    return event.get('Records')[0].get('Sns').get('Message')
+
+
+def _publish_event_to_pagerduty(msg: str,
+                                integration_key: str = PD_INT_KEY,
+                                severity: str = PD_SEVERITY,
+                                source: str = PD_SOURCE) -> dict:
+    '''Publish a message to the PagerDuty API'''
+    r = pypd.EventV2.create(
+        data={
+            'routing_key': integration_key,
+            'event_action': 'trigger',
+            'payload': {
+                'summary': msg,
+                'severity': severity,
+                'source': source,
+            }
+        }
+    )
+    return r
+
+
+def handler(event, context):
+    '''Function entry'''
+    _logger.debug('Event received: {}'.format(json.dumps(event)))
+
+    msg = _get_message_from_event(event)
+
+    pagerduty_response = _publish_event_to_pagerduty(msg)
+
+    resp = {
+        'pagerduty_response': pagerduty_response,
+        'status': 'OK'
+    }
+
+    _logger.debug('Response: {}'.format(json.dumps(resp)))
+    return resp
+
